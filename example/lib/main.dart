510d6d442736b65bf2aa8a9d53d1a5061696ee8a
Ben Meyer-Meisel <ben.meyermeisel@gmail.com>
2020-07-04 12:56:25 +0200

    AndroidX Migration

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -31,96 +31,95 @@ class _MyHomePageState extends State<MyHomePage> {
 
   static final dateInfoHeight = 11.0;
   final Map<DateTime, Widget> unselectedWeather = {
-    for (int i = 0; i < 10; i++)
-      ...{
-        DateTime.now().add(Duration(days: i)): Row(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            Icon(
-              Icons.wb_sunny,
+    for (int i = 0; i < 10; i++) ...{
+      DateTime.now().add(Duration(days: i)): Row(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: <Widget>[
+          Icon(
+            Icons.wb_sunny,
+            color: Colors.yellow,
+            size: dateInfoHeight,
+          ),
+          Text(
+            "20°C",
+            style: TextStyle(
               color: Colors.yellow,
-              size: dateInfoHeight,
+              fontSize: dateInfoHeight,
             ),
-            Text(
-              "20°C",
-              style: TextStyle(
-                color: Colors.yellow,
-                fontSize: dateInfoHeight,
-              ),
-            ),
-          ],
-        ),
-      }
+          ),
+        ],
+      ),
+    }
   };
 
   final Map<DateTime, Widget> selectedWeather = {
-    for (int i = 0; i < 10; i++)
-      ...{
-        DateTime.now().add(Duration(days: i)): Row(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            Icon(
-              Icons.ac_unit,
+    for (int i = 0; i < 10; i++) ...{
+      DateTime.now().add(Duration(days: i)): Row(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: <Widget>[
+          Icon(
+            Icons.ac_unit,
+            color: Colors.blue,
+            size: dateInfoHeight,
+          ),
+          Text(
+            "10°C",
+            style: TextStyle(
               color: Colors.blue,
-              size: dateInfoHeight,
+              fontSize: dateInfoHeight,
             ),
-            Text(
-              "10°C",
-              style: TextStyle(
-                color: Colors.blue,
-                fontSize: dateInfoHeight,
-              ),
-            ),
-          ],
-        ),
-      }
+          ),
+        ],
+      ),
+    }
   };
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        floatingActionButton: FloatingActionButton(
-          child: Icon(Icons.replay),
-          onPressed: () {
-            _controller.animateToSelection();
-          },
+      floatingActionButton: FloatingActionButton(
+        child: Icon(Icons.replay),
+        onPressed: () {
+          _controller.animateToSelection();
+        },
+      ),
+      appBar: AppBar(
+        title: Text(widget.title),
+      ),
+      body: Container(
+        padding: EdgeInsets.all(20.0),
+        color: Colors.blueGrey[100],
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            Text("You Selected:"),
+            Padding(
+              padding: EdgeInsets.all(10),
+            ),
+            Text(_selectedValue.toString()),
+            Padding(
+              padding: EdgeInsets.all(20),
+            ),
+            Container(
+              child: DatePicker(
+                DateTime.now(),
+                height: 90,
+                controller: _controller,
+                initialSelectedDate: DateTime.now(),
+                onDateChange: (date) {
+                  // New date selected
+                  setState(() {
+                    _selectedValue = date;
+                  });
+                },
+                selectedDateInfo: selectedWeather,
+                unselectedDateInfo: unselectedWeather,
+                dateInfoHeight: dateInfoHeight,
+              ),
+            ),
+          ],
         ),
-        appBar: AppBar(
-          title: Text(widget.title),
-        ),
-        body: Container(
-          padding: EdgeInsets.all(20.0),
-          color: Colors.blueGrey[100],
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: <Widget>[
-              Text("You Selected:"),
-              Padding(
-                padding: EdgeInsets.all(10),
-              ),
-              Text(_selectedValue.toString()),
-              Padding(
-                padding: EdgeInsets.all(20),
-              ),
-              Container(
-                child: DatePicker(
-                  DateTime.now(),
-                  height: 90,
-                  controller: _controller,
-                  initialSelectedDate: DateTime.now(),
-                  onDateChange: (date) {
-                    // New date selected
-                    setState(() {
-                      _selectedValue = date;
-                    });
-                  },
-                  selectedDateInfo: selectedWeather,
-                  unselectedDateInfo: unselectedWeather,
-                  dateInfoHeight: dateInfoHeight,
-                ),
-              ),
-            ],
-          ),
-        ));
+      ),
+    );
   }
 }
